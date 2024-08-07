3464e34196b3c712c01a3c35d83cfc78da64877a
Jose Daniel Cabrera Calderón <162157418+Jdcabreradev@users.noreply.github.com>
2024-08-07 00:10:30 -0500

    feat: :bookmark: New version release that includes log saving in the so
    new parameters added to construct the logger, and new feature for saving log files
    
    change logger struct, save log files in the system

---
+++ test/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"time"
+
+	"github.com/Jdcabreradev/logify"
+)
+
+func main() {
+	// Create a logger that will save logs to a new file with a timestamp
+	logger := logify.NewLogger("MyModule", "MyConsumer", "development", "./logs/")
+	defer logger.Close() // Ensure the log file is closed when done
+
+	logger.Log(logify.INFO, "This is an informational message.")
+
+	// Simulate application execution
+	time.Sleep(2 * time.Second)
+
+	logger.Log(logify.ERROR, "This is an error message.")
+}
