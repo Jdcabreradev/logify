0647f7593e8c90ecbba076a556e6b33a70e6e33c
Jose Daniel Cabrera Calderón <162157418+Jdcabreradev@users.noreply.github.com>
2024-08-07 01:03:41 -0500

    feat: :bookmark: 2.0 release fixed
    fixed 2.0 released

--- colors.go
+++ colors.go
@@ -1,19 +1,29 @@
 package logify
 
 // LogColor holds the colors for each log type.
+// LogColor is a map that associates each log type with a specific color for console output.
 var LogColor = map[LogType]string{
-	INFO:    Green,
-	WARNING: Yellow,
-	ERROR:   Red,
-	DEBUG:   Blue,
+	INFO:    Green,  // The color green is used for INFO log types.
+	WARNING: Yellow, // The color yellow is used for WARNING log types.
+	ERROR:   Red,    // The color red is used for ERROR log types.
+	DEBUG:   Blue,   // The color blue is used for DEBUG log types.
 }
 
 // SetColor allows changing the color for a specific log type.
+// SetColor enables changing the color associated with a specific log type.
 func SetColor(logType LogType, color string) {
+	// Map of valid colors. The color must be in this map to be considered valid.
 	if _, valid := map[string]bool{
-		Black: true, Red: true, Green: true, Yellow: true,
-		Blue: true, Magenta: true, Cyan: true, White: true,
+		Black:   true, // Valid black color.
+		Red:     true, // Valid red color.
+		Green:   true, // Valid green color.
+		Yellow:  true, // Valid yellow color.
+		Blue:    true, // Valid blue color.
+		Magenta: true, // Valid magenta color.
+		Cyan:    true, // Valid cyan color.
+		White:   true, // Valid white color.
 	}[color]; valid {
+		// If the color is valid, update the color associated with the log type in LogColor.
 		LogColor[logType] = color
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Jdcabreradev/logify
+module github.com/Jdcabreradev/logify/v2
 
 go 1.22.5
